22b0807d37a9e4b2e4094e3dd31e211860b56b86
Sabbir Ahmed <sabbirahmed@Sabbirs-MacBook-Pro.local>
2021-02-07 12:50:54 +0600

    GOlang practics

---
+++ veriable.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "Sabbir"
+	// var name2, city string = "Ahmed", "Dhaka"
+	// fmt.Println(name, name2, city)
+
+
+	// char
+	var c rune = 'A'
+	var age int = 10
+	var result float32 = 50.95
+
+	fmt.Println(c, age, result)
+}

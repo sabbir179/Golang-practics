4067a9ebac467eeb5ce16fba43397c750f3eb201
Sabbir Ahmed <sabbirahmed@Sabbirs-MacBook-Pro.local>
2021-02-07 13:17:48 +0600

    GOlang primitive data types practics

--- main.go
+++ main.go
@@ -3,5 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Bangladesh!")
+
+	//var name string
+	// var age int
+	// var result float32
+	// var isFound bool
+
+	// fmt.Println(name)
+	// fmt.Println(age)
+	// fmt.Println(result)
+
+	var chr rune
+	chr = 'u'
+	fmt.Println(chr)
+	fmt.Println("%c", chr)
+
+	// fmt.Println(isFound)
+	// fmt.Println(true && true)
+	// fmt.Println(true && false)
+	// fmt.Println(true || true)
+	// fmt.Println(true || false)
+	// fmt.Println(true)
+
 }

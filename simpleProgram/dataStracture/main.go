1bba681b1d94b61ca534919ebc6cf142b3b40a33
Sabbir Ahmed <sabbirahmed@Sabbirs-MacBook-Pro.local>
2021-02-09 00:25:27 +0600

    data structure

---
+++ simpleProgram/dataStracture/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// primitive data types
+	// rune, byte = alias
+	// int, float32, string, bool
+
+	// Composite Data Types Maps,
+	// array,
+
+	var students [3]string
+	// rules of  array
+	// fixed lenght ,
+	//same type
+
+	students[0] = "Salam"
+	students[1] = "Scaymam"
+	students[2] = "Aalam"
+
+	//fmt.Println(students)
+	//fmt.Println(len(students))
+	fmt.Println(students[2])
+
+}

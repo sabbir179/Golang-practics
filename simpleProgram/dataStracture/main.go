811e855a5a5e824f18a8d959d0cc04c719421c17
Sabbir Ahmed <sabbirahmed@Sabbirs-MacBook-Pro.local>
2021-02-09 09:31:48 +0600

    data structure

--- simpleProgram/dataStracture/main.go
+++ simpleProgram/dataStracture/main.go
@@ -11,17 +11,26 @@ func main() {
 	// Composite Data Types Maps,
 	// array,
 
-	var students [3]string
+	// var students [3]string
 	// rules of  array
 	// fixed lenght ,
 	//same type
 
-	students[0] = "Salam"
-	students[1] = "Scaymam"
-	students[2] = "Aalam"
+	// data set or data assign
+	// students[0] = "Salam"
+	// students[1] = "Scaymam"
+	// students[2] = "Aalam"
 
 	//fmt.Println(students)
 	//fmt.Println(len(students))
-	fmt.Println(students[2])
+
+	//data pull, data retrieve, data get
+	//fmt.Println(students[2])
+
+	// short hand way, string literals
+	//In computer science, a literal is a notation for representing a fixed value in source code
+	// implicit = ... in []
+	students := [...]string{"Asmal", "Dhaka", "Bangladesh", "Canada"}
+	fmt.Println(students)
 
 }

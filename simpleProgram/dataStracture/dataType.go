45d5f930a10d6b9f548f8280236921f869c68a50
Sabbir Ahmed <sabbirahmed@Sabbirs-MacBook-Pro.local>
2021-02-15 15:51:57 +0600

    data type struct

--- simpleProgram/dataStracture/dataType.go
+++ simpleProgram/dataStracture/dataType.go
@@ -3,13 +3,14 @@ package main
 import "fmt"
 
 // make structure
-type Book struct {
-	Title  string
-	Author string
-	ISBN   string
-	Price  float32
-	Pages  int
-}
+
+// type Book struct {
+// 	Title  string
+// 	Author string
+// 	ISBN   string
+// 	Price  float32
+// 	Pages  int
+// }
 
 func main() {
 
@@ -17,15 +18,35 @@ func main() {
 	// slice
 	// map
 	// struct
+	// user defined data types or composite data type or
 	// a struct is a collection of fields or properties.
 
 	// assign value
 
-	var b1 Book
-	b1.Title = "An Introduction to programming in GO"
-	b1.Author = "CALEB DOXY"
-	b1.Price = 10.50
-	b1.Pages = 165
+	// var b1 Book
+	// b1.Title = "An Introduction to programming in GO"
+	// b1.Author = "CALEB DOXY"
+	// b1.Price = 10.50
+	// b1.Pages = 165
+
+	// struct literals : In computer science, a literal is a notation for representing a fixed value in source code.
+	// example: var name string = "Sabbir"
+
+	// Annonymous stuct
+
+	b1 := struct {
+		Title  string
+		Author string
+		ISBN   string
+		Price  float32
+		Pages  int
+	}{
+		Title:  "An Introduction to programming in GO",
+		Author: "CALEB DOXY",
+		ISBN:   "987-1478355823",
+		Price:  10.50,
+		Pages:  165,
+	}
 
 	fmt.Println(b1)
 	fmt.Println(b1.Title)
